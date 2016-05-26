f411847240c5eca095328305b5868182ffa37820
Egor <egor.kozitski@mail.citytech.cuny.edu>
2016-05-26 00:45:43 -0400

    Added iron route

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,5 +1,12 @@
 import {Template} from 'meteor/templating'
-
-
+import {Session} from 'meteor/session'
 
 import './body.html';
+
+Router.route('/sign',function (){
+	this.render('sign_in');
+})
+
+Router.route('/register',function (){
+	this.render('sign_up');
+})
